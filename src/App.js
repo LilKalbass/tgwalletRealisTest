98ac8cdd890c9cf368c8c04f00259f8b83b1935e
Artem Dimoglov <adimoglov@gmail.com>
2025-01-16 14:31:50 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <TonConnectUIProvider manifestUrl = "https://tgwallet-realis-test.vercel.app/tonconnect-manifest.json">
+        <div className="App">
+          <header className="App-header">
+          </header>
+            <p>qwe</p>
+        </div>
+      </TonConnectUIProvider>
   );
 }
 
